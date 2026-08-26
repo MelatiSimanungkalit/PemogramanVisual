101515f4e6f23135e1730697e98a17a1a7a73489
MelatiSimanungkalit <melatisimanungkalit735@gmail.com>
2026-08-26 11:51:09 +0700

    Pertemuan 2 - Komponen Visual

---
+++ pertemuan2-Komponen-visual/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub txtNama_Click(sender As Object, e As EventArgs) Handles lblNama.Click
+
+    End Sub
+
+    Private Sub BtnTampilkan_Click(sender As Object, e As EventArgs) Handles BtnTampilkan.Click
+        MessageBox.Show("Halo dunia" & vbCrLf &
+            "Nama  : " & txtNama.Text & vbCrLf &
+            "NIM   : " & txtNIM.Text & vbCrLf &
+            "KOM   : " & txtKom.Text
+                        )
+    End Sub
+
+    Private Sub TextNama_TextChanged(sender As Object, e As EventArgs) Handles txtNama.TextChanged
+
+    End Sub
+
+    Private Sub btnhapus_Click(sender As Object, e As EventArgs) Handles BtnHAPUS.Click
+        txtNama.Clear()
+        txtNIM.Clear()
+        txtKom.Clear()
+
+    End Sub
+
+    Private Sub txtKeluar_Click(sender As Object, e As EventArgs) Handles btnKeluar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
